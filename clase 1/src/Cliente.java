e609be4968a2cede0b1b58fd188b2676e4c94655
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-08-05 20:06:46 -0500

    Update Cliente.java

--- clase 1/src/Cliente.java
+++ clase 1/src/Cliente.java
@@ -1,3 +1,43 @@
+
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Cliente {
+    
+    public static void main( String args [] ) throws Exception{
+        
+        ObjectInputStream objectInputStream = null;
+        ObjectOutputStream objectOutputStream = null;
+        
+        Socket socket = null;
+        
+        try {
+            socket = new Socket("127.0.0.1", 5432);
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            
+            objectOutputStream.writeObject("Juan Pablo Florez Caicedo");
+            
+            String respuesta = (String) objectInputStream.readObject();
+            System.out.println("respuesta: " + respuesta);
+            
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            if(objectOutputStream != null){
+                    objectOutputStream.close();
+                }
+                if(objectInputStream != null){
+                    objectInputStream.close();
+                }
+                if(socket != null){
+                    socket.close();
+                }
+                System.out.println("Conexion cerrada o terminada");
+        }
+    }
     
 }

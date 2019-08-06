ae07864d97adb7a61c171a45178bbcd9d384b6c9
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-08-05 19:41:37 -0500

    creacion del servicio en el servidor
    teniendo encuenta que el cliente, nos envia su nombre, nosotros lo saludaremos desde el servidor

--- clase 1/src/Servidor.java
+++ clase 1/src/Servidor.java
@@ -8,11 +8,35 @@ public class Servidor {
     
     public static void main (String args []) throws Exception{
         
+        //objeto de escucha
         ObjectInputStream objectInputStream = null;
+        //objeto de envio de informacion
         ObjectOutputStream objectOutputStream = null;
+        //socket
         Socket socket = null;
+        //socket con puerto de escucha
         ServerSocket serverSocket = new ServerSocket(5432);
         
+        while(true) {
+            //escucha el puerto 5432
+            socket = serverSocket.accept();
+            //imprime la ip del mensaje que fue recibido en el puerto
+            System.out.println("\nSe conectaron desde la IP: " + socket.getInetAddress()); 
+            //obtencion del mensaje
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            //salida del mensaje
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            
+            //desempaquetado de informacion para la obtencion de datos del mensaje
+            String nombre = (String) objectInputStream.readObject();
+            //saludo al cliete, con su nombre y hora de recibido en el servidor
+            String saludo = "Hola " + nombre + " tiempo: " + System.currentTimeMillis();
+            
+            //envio del mensaje
+            objectOutputStream.writeObject(saludo);
+            //confirmacion de envio de mensaje en consola
+            System.out.println("enviando mensaje al cliente");
+        }
         
     }
     

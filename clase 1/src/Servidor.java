070a8b0265561387084d660bc2fdd3c50fdd80f1
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-08-05 19:27:50 -0500

    creacion de objetos de escucha del servidor

--- clase 1/src/Servidor.java
+++ clase 1/src/Servidor.java
@@ -1,3 +1,19 @@
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+    
+    public static void main (String args []) throws Exception{
+        
+        ObjectInputStream objectInputStream = null;
+        ObjectOutputStream objectOutputStream = null;
+        Socket socket = null;
+        ServerSocket serverSocket = new ServerSocket(5432);
+        
+        
+    }
     
 }

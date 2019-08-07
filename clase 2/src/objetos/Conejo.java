96d526ecfa0bc3350b7c3999793ac5eaf1e66c20
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-08-06 19:04:44 -0500

    ejercicio Set Sentimiento

--- clase 2/src/objetos/Conejo.java
+++ clase 2/src/objetos/Conejo.java
@@ -25,7 +25,26 @@ public class Conejo {
     }
 
     public void setSentimiento(String sentimiento) {
-        this.sentimiento = sentimiento;
+        String[][] sentimientos = new String[6][2];
+        String cara = this.sentimiento;
+        sentimientos[0][0]= "felicidad";
+        sentimientos[0][1]= "tristeza";
+        sentimientos[0][2]= "locura";
+        sentimientos[0][3]= "seriedad";
+        sentimientos[0][4]= "sospecha";
+        sentimientos[0][5]= "raro";
+        sentimientos[1][0]= "(='U'=)";
+        sentimientos[1][1]= "(=ToT=)";
+        sentimientos[1][2]= "(=O.o=)";
+        sentimientos[1][3]= "(=-_-=)";
+        sentimientos[1][4]= "(=¬_¬=)";
+        sentimientos[1][5]= "(=@_@=)";
+        for(int i=0; i<6;i++){
+            if(sentimientos[0][i].matches(sentimiento)){
+                cara = sentimientos[1][i];
+            }
+        this.sentimiento = cara;
+        }
     }
 
     public String getPatas() {
